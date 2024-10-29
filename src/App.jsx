fa8ece521a8334c1b97fe340c014f5e896144a5c
sirimal <sirimal2002@gmail.com>
2024-10-29 19:16:02 +0700

    Aboutme

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Experience from "./components/Experience";
-import AboutMe from "./components/Aboutme";
+import AboutMe from "./components/AboutMe";
 function App() {
   return (
     <div className="bg-gray-900">
